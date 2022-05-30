f44bdf9b34a1a5ab8a1193421b7946b284fc1f4d
Anandkalp <anandk.neo@gmail.com>
2022-05-30 23:31:30 +0530

    added

---
+++ sumy.java
@@ -0,0 +1,25 @@
+class Calculator
+{
+	int a, b;
+	int sum;
+		Calculator(int dosa, int boost)
+	{
+		a = dosa;
+		b = boost;
+		}
+		public void add()
+		{
+			sum = a+b;
+			System.out.println("sum = "+sum);
+		}
+		}
+		class CalcManager
+		{
+		public static void main(String args[])
+		{
+		Calculator calc = new Calculator(35, 18);
+		calc.add();
+	}
+}
+
+	
